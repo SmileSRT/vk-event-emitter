c44031aea5f63d068d8a94f147be1bbef12b213d
SmileSRT <korovichev367@gmail.com>
2025-01-20 15:24:49 +0300

    initial commit

---
+++ EventEmitter.ts
@@ -0,0 +1,47 @@
+type Listener = (...args: any[]) => void;
+
+export class EventEmitter {
+  events: Record<string, Listener[]>;
+
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName: string, listener: Listener) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(listener);
+  }
+
+  off(eventName: string, listener: Listener) {
+    this.events[eventName] = this.events[eventName].filter(
+      (currentListener) => currentListener !== listener
+    );
+  }
+
+  emit(eventName: string, ...args: any[]) {
+    const events = this.events[eventName];
+
+    if (events) {
+      for (const callback of events) {
+        callback.call(null, args);
+      }
+    }
+  }
+}
+
+const emitter = new EventEmitter();
+
+const logData = (data: Record<string, string>) => console.log(data);
+const errorData = (data: Record<string, string>) => console.error(data);
+
+emitter.on("data", logData);
+emitter.on("data", errorData);
+
+emitter.emit("data", { message: "Hello, world!" });
+
+emitter.off("data", errorData);
+
+emitter.emit("data", { message: "after" });
